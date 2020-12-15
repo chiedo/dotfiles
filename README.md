91d667aadfb1c0c3387e6cc38ec1aaa8d3948ad9
Chiedo John <chiedo@chiedosbmacbook.lan>
2020-12-15 10:31:43 -0500

    Progress

--- README.md
+++ README.md
@@ -6,4 +6,3 @@ My dotfiles for my development environment.
 
 1. Git clone this repo to the machine
 2. Use the symlink-dotfiles.sh script to automatically create symlinks for the dotfiles in this directory. By default and overwrite whatever you may have in that directory. `bash symlink-dotfiles.sh`
-3. When you open `vim`, run `:BundleInstall`
--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -6,17 +6,20 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Install vundle (required for the vim stuff)
 VUNDLE_GIT=https://github.com/VundleVim/Vundle.vim.git
-VUNDLE_TARGET=~/.vim/bundle/Vundle.vim
+VUNDLE_TARGET=$HOME/.vim/bundle/Vundle.vim
 if [ ! -d $VUNDLE_TARGET ]; then
     echo "Installing Vundle..."
+    mkdir $HOME/.vim
+    mkdir $HOME/.vim/bundle
     git clone $VUNDLE_GIT $VUNDLE_TARGET
+    vim -c ':BundleInstall' -c ":x" -c ":x"
 else
     echo "Vundle appears to already be installed."
 fi
 
 # Install oh-my-zsh (required for the vim stuff)
-OH_MY_ZSH_TARGET=~/.oh-my-zsh/oh-my-zsh.sh
-mv ~/.oh-my-zsh ~/oh-my-zsh.dotfile-backup
+OH_MY_ZSH_TARGET=$HOME/.oh-my-zsh/oh-my-zsh.sh
+mv $HOME/.oh-my-zsh $HOME/oh-my-zsh.dotfile-backup-$(date +%s)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 DOTFILES=(
@@ -32,16 +35,16 @@ DOTFILES=(
 )
 
 for dotfile in ${DOTFILES[@]}; do
-    if [ -h ~/$dotfile ]; then  # symlink
-        echo "~/$dotfile: overwriting existing symlink..."
-        ln -snf $DIR/$dotfile ~/$dotfile
-    elif [ -e ~/$dotfile ]; then  # any other file
-        echo "~/$dotfile: file exists, renaming to ~/$dotfile.dotfile-backup..."
-        mv ~/$dotfile ~/$dotfile.dotfile-backup
-        echo "~/$dotfile: creating symlink..."
-        ln -s $DIR/$dotfile ~/$dotfile
+    if [ -h $HOME/$dotfile ]; then  # symlink
+        echo "$HOME/$dotfile: overwriting existing symlink..."
+        ln -snf $DIR/$dotfile $HOME/$dotfile
+    elif [ -e $HOME/$dotfile ]; then  # any other file
+        echo "$HOME/$dotfile: file exists, renaming to $HOME/$dotfile.dotfile-backup..."
+        mv $HOME/$dotfile $HOME/$dotfile.dotfile-backup-$(date +%s)
+        echo "$HOME/$dotfile: creating symlink..."
+        ln -s $DIR/$dotfile $HOME/$dotfile
     else  # no file
-        echo "~/$dotfile: creating symlink..."
-        ln -s $DIR/$dotfile ~/$dotfile
+        echo "$HOME/$dotfile: creating symlink..."
+        ln -s $DIR/$dotfile $HOME/$dotfile
     fi
 done
