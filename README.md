f2e3ef028a3574713814279a667c91c6a1086e45
Chiedo John <chiedo@chiedosbmacbook.lan>
2020-12-15 10:20:40 -0500

    Checkpoint

--- README.md
+++ README.md
@@ -5,11 +5,7 @@ My dotfiles for my development environment.
 ## Setting up a new machine
 
 1. Git clone this repo to the machine
-2. Symlink each of the dotfiles needed to the ~/ directory for that machine.
-
-## How to symlink each dotfile
-
-The symlink-dotfiles.sh script is available to automatically create symlinks for the dotfiles in this directory. By default, it does not overwrite whatever you may have in that directory; you will need to delete any dotfiles in your home directory that you wish to autolink.
+2. Use the symlink-dotfiles.sh script to automatically create symlinks for the dotfiles in this directory. By default and overwrite whatever you may have in that directory.
 
 ```
 bash symlink-dotfiles.sh
--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -4,29 +4,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-DOTFILES=(
-    ".bash_aliases"
-    ".bash_functions"
-    ".bash_python_aliases"
-    ".gitconfig"
-    ".gitignore_global"
-    ".tmux.conf"
-    ".vim"
-    ".vimrc"
-)
-
-for dotfile in ${DOTFILES[@]}; do
-    if [ -h ~/$dotfile ]; then  # symlink
-        echo "~/$dotfile: overwriting existing symlink..."
-        ln -snf $DIR/$dotfile ~/$dotfile
-    elif [ -e ~/$dotfile ]; then  # any other file
-        echo "~/$dotfile: file exists, skipping..."
-    else  # no file
-        echo "~/$dotfile: creating symlink..."
-        ln -s $DIR/$dotfile ~/$dotfile
-    fi
-done
-
 # Install vundle (required for the vim stuff)
 VUNDLE_GIT=https://github.com/VundleVim/Vundle.vim.git
 VUNDLE_TARGET=~/.vim/bundle/Vundle.vim
@@ -36,3 +13,35 @@ if [ ! -d $VUNDLE_TARGET ]; then
 else
     echo "Vundle appears to already be installed."
 fi
+
+# Install oh-my-zsh (required for the vim stuff)
+OH_MY_ZSH_TARGET=~/.oh-my-zsh/oh-my-zsh.sh
+mv ~/.oh-my-zsh ~/oh-my-zsh.dotfile-backup
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+DOTFILES=(
+    ".bash_aliases"
+    ".bash_functions"
+    ".bash_python_aliases"
+    ".gitconfig"
+    ".gitignore_global"
+    ".tmux.conf"
+    ".vim"
+    ".vimrc"
+    ".zshrc"
+)
+
+for dotfile in ${DOTFILES[@]}; do
+    if [ -h ~/$dotfile ]; then  # symlink
+        echo "~/$dotfile: overwriting existing symlink..."
+        ln -snf $DIR/$dotfile ~/$dotfile
+    elif [ -e ~/$dotfile ]; then  # any other file
+        echo "~/$dotfile: file exists, renaming to ~/$dotfile.dotfile-backup..."
+        mv ~/$dotfile ~/$dotfile.dotfile-backup
+        echo "~/$dotfile: creating symlink..."
+        ln -s $DIR/$dotfile ~/$dotfile
+    else  # no file
+        echo "~/$dotfile: creating symlink..."
+        ln -s $DIR/$dotfile ~/$dotfile
+    fi
+done
